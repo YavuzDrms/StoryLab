db759eb7482fc6a17d9975c8767122dfdc4c38fd
Yavuz durmus <yavuzdurmus6134@gmail.com>
2026-02-26 18:35:51 +0300

    Input tests and random story

--- backend/app.py
+++ backend/app.py
@@ -1,23 +1,34 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+import random
+
+app = FastAPI()
 
 # Kullacının gireceği verinin modeli (test)
 class DataModel(BaseModel):
     text: str # Kullanıcının gireceği veri
 
-app = FastAPI()
+# Hikaye devamları (test) sonrasında bunu zaten ai yapacak
+fake_story = [
+    " Blablalbalblablablablablablalba",
+    " BNJRNBJSNRIJBNSŞKRIJNBJŞNSRJNB",
+    " HİKAYE DEVAMI"
+]
 
-@app.post("/test-data")
+@app.post("/create-story-test")
 def get_data(data: DataModel):
     message = data.text # Kullanıcının girdisi
     
     print(f'Text reached backend: {message}')
     
+    ai_story = random.choice(fake_story)
+    full_story = f'Story: {message} {ai_story}'
+    
     # Client e yanıt
     return {
         "situation" : "succesful",
-        "input" : message,
-        "input_lenght" : len(message)
+        "story" : full_story,
+        "story_lenght" : len(full_story)
     }
 
 @app.get("/")
